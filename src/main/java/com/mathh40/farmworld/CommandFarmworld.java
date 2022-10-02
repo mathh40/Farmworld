4d434333da4f8e001ff953ffed06366e30ebe208
CleanThat <CleanThat>
2022-10-02 22:16:47 +0000

    language=java nb_files_neither_included_nor_excluded: 1 nb_files_formatted: 3

--- src/main/java/com/mathh40/farmworld/CommandFarmworld.java
+++ src/main/java/com/mathh40/farmworld/CommandFarmworld.java
@@ -5,47 +5,39 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
 public class CommandFarmworld implements CommandExecutor {
 
-    private Farmworld plugin;
+  private Farmworld plugin;
 
-    CommandFarmworld(Farmworld plugin)
-    {
-        this.plugin = plugin;
-    }
+  CommandFarmworld(Farmworld plugin) {
+    this.plugin = plugin;
+  }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(args.length == 0)
-        {
-            if(sender instanceof Player)
-            {
-                Player player = (Player) sender;
-                player.teleport(plugin.getMVWorldManager().getMVWorld("farmworld").getSpawnLocation());
+  @Override
+  public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+      @NotNull String label, @NotNull String[] args) {
+    if (args.length == 0) {
+      if (sender instanceof Player) {
+        Player player = (Player) sender;
+        player.teleport(plugin.getMVWorldManager().getMVWorld("farmworld").getSpawnLocation());
+      } else {
+        plugin.getServer().getLogger().warning("This command cannot use on the Console");
+      }
+    } else if (args.length == 1) {
+      switch (args[0]) {
+        case "regen":
+          if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (player.hasPermission("farmworld.regen")) {
+              plugin.regenFarmworld();
             }
-            else
-            {
-                plugin.getServer().getLogger().warning("This command cannot use on the Console");
-            }
-        } else if (args.length == 1) {
-            switch (args[0])
-            {
-                case "regen":
-                    if(sender instanceof Player)
-                    {
-                        Player player = (Player) sender;
-                        if(player.hasPermission("farmworld.regen"))
-                        {
-                            plugin.regenFarmworld();
-                        }
-                    }
-                    else
-                    {
-                        plugin.regenFarmworld();
-                    }
-                    break;
-            }
-        }
-        return false;
+          } else {
+            plugin.regenFarmworld();
+          }
+          break;
+      }
     }
+    return false;
+  }
 }
