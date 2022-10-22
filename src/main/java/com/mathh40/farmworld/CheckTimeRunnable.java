cb20ad705f23dcb0b8e1809ccca47148aec4ea4e
CleanThat <CleanThat>
2022-10-22 01:08:45 +0000

    language=java nb_files_neither_included_nor_excluded: 3 nb_files_formatted: 4

--- src/main/java/com/mathh40/farmworld/CheckTimeRunnable.java
+++ src/main/java/com/mathh40/farmworld/CheckTimeRunnable.java
@@ -1,4 +1,5 @@
 package com.mathh40.farmworld;
+
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.time.Duration;
@@ -6,23 +7,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CheckTimeRunnable extends BukkitRunnable {
-    private final Farmworld plugin;
+  private final Farmworld plugin;
 
-    public CheckTimeRunnable(Farmworld plugin) {
-        this.plugin = plugin;
-    }
+  public CheckTimeRunnable(Farmworld plugin) {
+    this.plugin = plugin;
+  }
 
-    @Override
-    public void run() {
-        LocalDateTime  now = LocalDateTime.now();
-        for(FarmworldInstances instances : Farmworld.farmworlds.values()) {
-            String time = instances.getTime();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime last = LocalDateTime.parse(time, formatter);
-            Duration duration = Duration.between(now, last);
-            if (duration.toDays() == instances.getRegTime()) {
-                instances.regenFarmworld();
-            }
-        }
+  @Override
+  public void run() {
+    LocalDateTime now = LocalDateTime.now();
+    for (FarmworldInstances instances : Farmworld.farmworlds.values()) {
+      String time = instances.getTime();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+      LocalDateTime last = LocalDateTime.parse(time, formatter);
+      Duration duration = Duration.between(now, last);
+      if (duration.toDays() == instances.getRegTime()) {
+        instances.regenFarmworld();
+      }
     }
+  }
 }
